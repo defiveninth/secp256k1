cfe5cea0babade536459e570431bb725f5684d55
Abdurrauf <misterfighter1990@gmail.com>
2026-06-23 11:13:34 +0500

    init rrs-mobile

---
+++ rrs-mobile/rrs_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  rrs_mobileApp.swift
+//  rrs-mobile
+//
+//  Created by Abdurrauf on 23.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct rrs_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
